4968f288dd5b73a2aa4c4157d53d4afcf72b595e
sahinoor101 <sahinoor101@gmail.com>
2026-08-26 15:50:24 +0530

    Sudoku Class added, rowMask and columnMask method created

--- src/main.py
+++ src/main.py
@@ -1,36 +1,20 @@
-board = [
-    [5, 3, 0, 0, 7, 0, 0, 0, 0],
-    [6, 0, 0, 1, 9, 5, 0, 0, 0],
-    [0, 9, 8, 0, 0, 0, 0, 6, 0],
-    [8, 0, 0, 0, 6, 0, 0, 0, 3],
-    [4, 0, 0, 8, 0, 3, 0, 0, 1],
-    [7, 0, 0, 0, 2, 0, 0, 0, 6],
-    [0, 6, 0, 0, 0, 0, 2, 8, 0],
-    [0, 0, 0, 4, 1, 9, 0, 0, 5],
-    [0, 0, 0, 0, 8, 0, 0, 7, 9],
-]
+import methods
 
+
+
+print(methods.rowMask(0))
+print(methods.columnMask(2))
 boardBinary = []
 
-for x in board:
-    for y in x:
-        print(y, end=" ")
-    print()
 
-for x in board:
-    temp = []
-    i = 0
-    for y in x:
-        if y > 0:
-            temp.append(1)
-        else:
-            temp.append(0)
-    boardBinary.append(temp)
+# for x in board:
+#     temp = []
+#     i = 0
+#     for y in x:
+#         if y > 0:
+#             temp.append(1)
+#         else:
+#             temp.append(0)
+#     boardBinary.append(temp)
 
-print()
-print()
-for x in boardBinary:
-    for y in x:
-        print(y, end=" ")
-    print()
 
--- src/methods.py
+++ src/methods.py
@@ -1,13 +1,44 @@
-def rowMask():
-    pass
+#class 
+from colorsys import rgb_to_yiq
+class Sudoku:
+    board = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9],
+    ]
 
 
-def columnMask():
-    pass
-
-
+#Masking Functions
+s = Sudoku()
+def rowMask(row):
+    mask = 0
+    for r in s.board[row]:
+        if r > 0:
+            mask += 1 << r-1
+    return mask
+                
+def columnMask(col):
+    mask = 0
+    for r in range(9):
+        if(s.board[r][col])>0:
+            mask += 1 << s.board[r][col] -1
+    return mask
+                       
+                       
 def gridMask():
     pass
 
 def getShadow():
     pass 
+
+def displaySudoku(board):
+    for x in board:
+        for y in x:
+            print(y, end=" ")
+    print()
