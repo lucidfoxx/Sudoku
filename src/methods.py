faa76c41715b0446a1bcb39d14662096a299ee32
LucidFoxx <sahinoor.afiya@gmail.com>
2026-08-28 14:48:55 +0530

    Solver Function added (not working)

--- src/methods.py
+++ src/methods.py
@@ -1,25 +1,29 @@
+# imports
+
+import sys
+
+sys.setrecursionlimit(100000)
 # class
 
 
-class Sudoku:
-    board = [
-        [5, 3, 0, 0, 7, 0, 0, 0, 0],
-        [6, 0, 0, 1, 9, 5, 0, 0, 0],
-        [0, 9, 8, 0, 0, 0, 0, 6, 0],
-        [8, 0, 0, 0, 6, 0, 0, 0, 3],
-        [4, 0, 0, 8, 0, 3, 0, 0, 1],
-        [7, 0, 0, 0, 2, 0, 0, 0, 6],
-        [0, 6, 0, 0, 0, 0, 2, 8, 0],
-        [0, 0, 0, 4, 1, 9, 0, 0, 5],
-        [0, 0, 0, 0, 8, 0, 0, 7, 9],
-    ]
-    rowMaskValues = []
-    columnMaskValues = []
-    gridMaskValues = []
+board = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9],
+]
+rowMaskValues = []
+columnMaskValues = []
+gridMaskValues = []
 
+boardCopy = [r[:] for r in board]
 
 # Masking Functions
-s = Sudoku()
 
 
 def isValid(row, col):
@@ -36,77 +40,102 @@ def nextEmptySpace(board):
         for j in range(9):
             if board[i][j] == 0:
                 return (i, j)
-    return -1
+    return (-1, -1)
 
 
 def isValidNumber(row, col, num):
     if isValid(row, col):
-        if s.rowMaskValues[row] & num:
+        if rowMaskValues[row] & num:
             return False
-        elif s.columnMaskValues[col] & num:
+        elif columnMaskValues[col] & num:
             return False
-        elif s.gridMaskValues[(row // 3) * 3 + col // 3] & num:
+        elif gridMaskValues[(row // 3) * 3 + col // 3] & num:
             return False
         return True
     return False
 
 
-def rowMask(row):
+def rowMask(row, arr):
     mask = 0
-    for r in s.board[row]:
+    for r in arr[row]:
         if r > 0:
             mask += 1 << r - 1
     return mask
 
 
-def columnMask(col):
+def columnMask(col, arr):
     mask = 0
     for r in range(9):
-        if (s.board[r][col]) > 0:
-            mask += 1 << s.board[r][col] - 1
+        if (arr[r][col]) > 0:
+            mask += 1 << arr[r][col] - 1
     return mask
 
 
-def gridMask(gridIndex):
+def gridMask(gridIndex, arr):
     mask = 0
     if gridIndex < 3:
         for i in range(3):
             for j in range(3):
-                if s.board[i][gridIndex * 3 + j] > 0:
-                    mask += 1 << s.board[i][gridIndex * 3 + j] - 1
+                if arr[i][gridIndex * 3 + j] > 0:
+                    mask += 1 << arr[i][gridIndex * 3 + j] - 1
     elif gridIndex < 6:
         for i in range(3, 6):
             for j in range(3):
-                if s.board[i][(gridIndex - 3) * 3 + j] > 0:
-                    mask += 1 << s.board[i][(gridIndex - 3) * 3 + j] - 1
+                if arr[i][(gridIndex - 3) * 3 + j] > 0:
+                    mask += 1 << arr[i][(gridIndex - 3) * 3 + j] - 1
     else:
         for i in range(6, 9):
             for j in range(3):
-                if s.board[i][(gridIndex - 6) * 3 + j] > 0:
-                    mask += 1 << s.board[i][(gridIndex - 6) * 3 + j] - 1
+                if arr[i][(gridIndex - 6) * 3 + j] > 0:
+                    mask += 1 << arr[i][(gridIndex - 6) * 3 + j] - 1
 
     return mask
 
 
-def getShadow():
-    pass
-
-
-def initializeMask():
+def updateMask(arr):
+    global rowMaskValues
+    rowMaskValues = []
+    global columnMaskValues
+    columnMaskValues = []
+    global gridMaskValues
+    gridMaskValues = []
     for i in range(9):
-        s.rowMaskValues.append(rowMask(i))
-        s.columnMaskValues.append(columnMask(i))
-        s.gridMaskValues.append(gridMask(i))
+        rowMaskValues.append(rowMask(i, arr))
+        columnMaskValues.append(columnMask(i, arr))
+        gridMaskValues.append(gridMask(i, arr))
 
 
 def displayMask():
-    print(s.rowMaskValues)
-    print(s.columnMaskValues)
-    print(s.gridMaskValues)
+    print(rowMaskValues)
+    print(columnMaskValues)
+    print(gridMaskValues)
 
 
 def displaySudoku(board):
     for x in board:
         for y in x:
-            print(y, end=" ")
+            print(y , end=" ")
     print()
+
+
+# Solver
+
+
+def solve(tboard):
+    updateMask(tboard)
+    row, col = nextEmptySpace(tboard)
+    print(row, col)
+    if row == -1:
+        return tboard
+    for i in range(1, 10):
+        if isValidNumber(row, col, i):
+            tboard[row][col] = i
+            displaySudoku(tboard)
+            print("------")
+            solve(tboard)
+            tboard[row][col] = 0
+    return [[]]
+
+
+sboard = solve(boardCopy)
+displaySudoku(sboard)
